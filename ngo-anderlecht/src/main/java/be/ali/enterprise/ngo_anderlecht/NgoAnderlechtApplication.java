03349c142965a84c4acadf86e92ac955ca335911
aliasaad22 <ali.asaad1@student.ehb.be>
2026-05-28 22:53:55 +0200

    spring boot installeren en location map maken

---
+++ ngo-anderlecht/src/main/java/be/ali/enterprise/ngo_anderlecht/NgoAnderlechtApplication.java
@@ -0,0 +1,13 @@
+package ali.enterprise.be.ngo_anderlecht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgoAnderlechtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgoAnderlechtApplication.class, args);
+	}
+
+}
---
+++ ngo-anderlecht/src/test/java/ali/enterprise/be/ngo_anderlecht/NgoAnderlechtApplicationTests.java
@@ -0,0 +1,13 @@
+package ali.enterprise.be.ngo_anderlecht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgoAnderlechtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
